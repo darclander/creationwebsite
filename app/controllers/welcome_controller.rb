98c6f17592efb1e882481849b13a967c15b36039
Luke Darclander <lukedarclander@gmail.com>
2019-06-09 20:00:45 +0200

    first commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,13 @@
+class WelcomeController < ApplicationController
+  def index
+  end
+
+  def test
+  end
+  
+  def projects
+  end
+
+  def about
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  resources :welcome
+  root to: "welcome#index"
+  
+  get '/test', to: 'welcome#test'
+  get '/projects', to: 'welcome#projects'
+  get '/about', to: 'welcome#about'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
