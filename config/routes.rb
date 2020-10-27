f75d597eea9b0c12e3cdbbc276537fa760d94320
Darclander <lukedarclander@gmail.com>
2020-10-27 15:34:52 +0100

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   get '/test', to: 'welcome#test'
   get '/projects', to: 'welcome#projects'
   get '/about', to: 'welcome#about'
+  get '/starbg', to: 'welcome#starbg'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
